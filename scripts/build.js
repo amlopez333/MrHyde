85d141abcf311ec46611ab1956cb8a2a5dbf66a4
amlopez333 <amlopez333@gmail.com>
2018-02-26 10:37:32 -0600

    feat: added support for render in markdown and plain html

--- scripts/build.js
+++ scripts/build.js
@@ -1,24 +1,16 @@
 const {promisify} = require('util');
 const fsextra = require('fs-extra');
 const path = require('path');
-const ejsRenderFile = promisify(require('ejs').renderFile);
+const ejs = require('ejs');
+const frontMatter = require('front-matter');
+const marked = require('marked');
+const ejsRenderFile = promisify(ejs.renderFile);
 const promisifiedGlob = promisify(require('glob'));
 
 const srcPath = './src';
-const distPath = './public';
 
 
-
-const buildSite = function(){
-    const projectConfig = require('../src/config/project.config');
-    const config = require('../src/config/site.config');
-    console.log(`Emptying ${distPath}`)
-    fsextra.emptyDirSync(distPath);
-    console.log(`Emptied ${distPath}`)
-//check for sass and less
-    console.log(`Copying ${srcPath}/assets to ${distPath}/assets`)
-    fsextra.copy(`${srcPath}/assets`, `${distPath}/assets`);
-    console.log(`Copied ${srcPath}/assets to ${distPath}/assets`)
+const render = function(distPath, config){
     promisifiedGlob('**/*.@(ejs|md|html)', {cwd: `${srcPath}/pages`})
     .then(function(files){
         files.forEach(function(file){
@@ -26,12 +18,27 @@ const buildSite = function(){
             const destPath = path.join(distPath, fileData.dir);
             fsextra.mkdirs(destPath)
             .then(function(){
-                console.log(`Rendering ${fileData.dir}/${fileData.name}${fileData.ext}`)
-                return ejsRenderFile(`${srcPath}/pages/${file}`, Object.assign({}, config));
+                return fsextra.readFile(`${srcPath}/pages/${file}`, 'utf-8');
             })
-            .then(function(renderedPage){
-                console.log(`Applying layout to rendered ${fileData.dir}/${fileData.name}`)
-                return ejsRenderFile(`${srcPath}/layouts/layout.ejs`, Object.assign({}, config, { body: renderedPage}));
+            .then(function(data){
+                //considerar casos de archihvo y eso. 
+                const pageData = frontMatter(data);
+                const templateConfig = Object.assign({}, config, {page: pageData.attributes});
+                console.log(`Rendering ${fileData.dir}/${fileData.name}${fileData.ext}`);
+                let content;
+                switch(fileData.ext){
+                    case '.md':
+                        content = marked(pageData.body);
+                        break;
+                    case '.ejs':
+                        content = ejs.render(pageData.body, templateConfig);
+                        break;
+                    default:
+                        content = pageData.body;
+                }
+                const layout = pageData.attributes.layout || 'default';
+                console.log(`Applying ${layout} to rendered ${fileData.dir}/${fileData.name}${fileData.ext}`)
+                return ejsRenderFile(`${srcPath}/layouts/${layout}.ejs`, Object.assign({}, templateConfig, { body: content }));
             })
             .then(function(renderedLayout){
                 console.log(`Saving ${fileData.dir}/${fileData.name}.html`)
@@ -45,6 +52,27 @@ const buildSite = function(){
     .catch(function(error){
         console.log(error);
     })
+}
+
+const buildSite = function(){
+    const projectConfig = require('../src/config/project.config');
+    const config = require('../src/config/site.config');
+    const distPath = projectConfig.build.path || './public';
+    console.log(`Emptying ${distPath}`);
+    fsextra.emptyDirSync(distPath);
+    console.log(`Emptied ${distPath}`);
+//check for sass and less
+    console.log(`Copying ${srcPath}/assets to ${distPath}/assets`)
+    fsextra.copy(`${srcPath}/assets`, `${distPath}/assets`)
+    .then(function(){
+        console.log(`Copied ${srcPath}/assets to ${distPath}/assets`);
+        return render(distPath, config)
+    })
+    .catch(function(error){
+        console.log(`Error copying ${srcPath}/assets to ${distPath}/assets. The error was ${error.message}. Aborting build.`)
+    })
+    
+    
 }
 
 
